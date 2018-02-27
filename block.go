4f8afff86454bb6da44d5797ec5f806fd68e508f
gaoqiankun <1975210542@qq.com>
2018-02-27 16:57:05 +0800

    基本原型

--- block.go
+++ block.go
@@ -3,74 +3,33 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
-	"encoding/gob"
-	"log"
+	//	"fmt"
+	"strconv"
 	"time"
 )
 
-// Block keeps block headers
 type Block struct {
-	Timestamp    int64
-	Transactions []*Transaction
-
-	//	Data          []byte
-	PrevBlockHash []byte
-	Hash          []byte
-	Nonce         int
+	Timestamp     int64  //当前时间戳
+	Data          []byte //区块存储的实际有效信息
+	PrevBlockHash []byte //前一个块的hash
+	Hash          []byte //当前的hash
 }
 
-// Serialize serializes the block
-func (b *Block) Serialize() []byte {
-	var result bytes.Buffer
-	encoder := gob.NewEncoder(&result)
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
 
-	err := encoder.Encode(b)
-	if err != nil {
-		log.Panic(err)
-	}
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
 
-	return result.Bytes()
 }
 
-// NewBlock creates and returns Block
-func NewBlock(transactions []*Transaction, prevBlockHash []byte) *Block {
-
-	block := &Block{time.Now().Unix(), transactions, prevBlockHash, []byte{}, 0}
-	pow := NewProofOfWork(block)
-	nonce, hash := pow.Run()
-
-	block.Hash = hash[:]
-	block.Nonce = nonce
-
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
 	return block
 }
 
-// NewGenesisBlock creates and returns genesis Block
-func NewGenesisBlock(coinbase *Transaction) *Block {
-	return NewBlock([]*Transaction{coinbase}, []byte{})
-}
-
-// DeserializeBlock deserializes a block
-func DeserializeBlock(d []byte) *Block {
-	var block Block
-
-	decoder := gob.NewDecoder(bytes.NewReader(d))
-	err := decoder.Decode(&block)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	return &block
-}
-
-func (b *Block) HashTransactions() []byte {
-	var txHashes [][]byte
-	var txHash [32]byte
-
-	for _, tx := range b.Transactions {
-		txHashes = append(txHashes, tx.ID)
-	}
-	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
-	return txHash[:]
-
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
 }
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
-	cli := CLI{}
-	cli.Run()
+func main() {
+	bc := NewBlockchain()
+
+	bc.AddBlock("Send 1 BTC to Ivan")
+	bc.AddBlock("Send 2 BTC to Ivan")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
 }
